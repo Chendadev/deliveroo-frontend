3c0d42727cc287cbbe70ff3fcf25a5c4e3420c9a
Net Chenda <netchenda@macbook-pro-de-net.home>
2022-05-02 18:28:01 +0200

    display only the first name's restaurant

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>test</p>
-    </div>
-  );
-}
+import { useEffect, useState } from "react";
+import axios from "axios";
 
+function App() {
+
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // console.log("on est ds le use effect")
+    const fetchData = async () => {
+      const response = await axios.get("https://deliveroo-backend-chenda.herokuapp.com/")
+      // console.log(response.data.restaurant.categories)
+      setData(response.data);
+      setIsLoading(false);
+    }
+    fetchData()
+  }, []);
+
+  return (
+    isLoading === true ? (
+      <span>En cours de chargement... </span>
+    ) : (
+      <h2>{data.restaurant.name}</h2>
+    )
+  )
+}
 export default App;
